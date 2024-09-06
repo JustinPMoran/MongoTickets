d17d624aa60bdc42d1351ac62af5559beef9bfcf
pnleguizamo <pnleguizamo@gmail.com>
2024-09-06 10:43:48 -0500

    Init first experiment

---
+++ Experiments/nickExperiment/tutorials-springboot_unit1_1_helloworld/tutorials-springboot_unit1_1_helloworld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -0,0 +1,24 @@
+package coms309;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello and welcome to COMS 309: " + name;
+    }
+
+    @GetMapping("/new_route")
+    public String new_route() {
+        return "This is my new route in the Welcome Controller";
+    }
+}
