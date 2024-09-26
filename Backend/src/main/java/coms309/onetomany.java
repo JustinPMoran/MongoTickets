deecf39f76f46e9bfe45e514917756d34aa228d7
Anoop Boyal <anoop04@iastate.edu>
2024-09-26 03:43:44 +0000

    Update file onetomany.java

---
+++ Backend/src/main/java/coms309/onetomany.java
@@ -0,0 +1,58 @@
+package coms309;
+
+
+import coms309.Application;
+import coms309.Tickets.Ticket;
+import coms309.Tickets.TicketRepository;
+import coms309.Users.User;
+import coms309.Users.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+public class onetomany {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // Create 3 users with their machines
+
+    /**
+     * @param userRepository   repository for the User entity
+     * @param ticketRepository repository for the Ticket entity
+     *                         Creates a commandLine runner to enter dummy data into the database
+     *                         As mentioned in User.java just associating the Ticket object with the User will save it into the database because of the CascadeType
+     */
+    @Bean
+    CommandLineRunner initUser(UserRepository userRepository, TicketRepository ticketRepository) {
+        return args -> {
+            User user1 = new User("Anoop", "anoop@iastate.edu");
+            User user2 = new User("Nick", "nick@iastate.edu");
+            User user3 = new User("Justin", "justin@iastate.edu");
+            Ticket ticket1 = new Ticket("Football", "Sep 23", "G7", 8);
+            Ticket ticket2 = new Ticket("Volleyball", "Sep 24", "A2", 3);
+            Ticket ticket3 = new Ticket("Basketball", "Oct 15", "A11", 1);
+            Ticket ticket4 = new Ticket("Soccer", "May 3", "A8", 1);
+            Ticket ticket5 = new Ticket("Football", "Sep 23", "G8", 8);
+            Ticket ticket6 = new Ticket("Baseball", "Sep 22", "A1", 4);
+
+            user1.setTicket(ticket1);
+            user1.setTicket(ticket2);
+            user1.setTicket(ticket5);
+            user2.setTicket(ticket6);
+            user2.setTicket(ticket4);
+            user3.setTicket(ticket3);
+
+            userRepository.save(user1);
+            userRepository.save(user2);
+            userRepository.save(user3);
+
+
+        };
+    }
+
+}
+
