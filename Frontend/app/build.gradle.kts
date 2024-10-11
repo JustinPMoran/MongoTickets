04209eda30e618b88b2f82d7cd9892ae68db3646
Justin Moran <129006629+JustinPMoran@users.noreply.github.com>
2024-10-10 22:06:20 -0500

    fixed errors I had

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 dependencies {
 
 
-    implementation("com.android.volley:volley:1.2.1")
+    implementation(libs.volley)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
